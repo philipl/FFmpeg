6cdee39cb97bffa95236a1167609257cd8fbdf0f
Lynne <dev@lynne.ee>
2023-04-30 00:19:08 +0200

    hack for nvidia drivers again

--- libavcodec/vulkan_decode.c
+++ libavcodec/vulkan_decode.c
@@ -693,7 +693,7 @@ repeat:
     usage->videoUsageHints = VK_VIDEO_DECODE_USAGE_DEFAULT_KHR;
 
     profile->sType               = VK_STRUCTURE_TYPE_VIDEO_PROFILE_INFO_KHR;
-    profile->pNext               = usage;
+    profile->pNext               = usage->pNext;
     profile->videoCodecOperation = vk_codec->decode_op;
     profile->chromaSubsampling   = ff_vk_subsampling_from_av_desc(desc);
     profile->lumaBitDepth        = ff_vk_depth_from_av_depth(desc->comp[0].depth);
